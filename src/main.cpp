03a45211450ac6d5f051b9f4960f318d803b4d62
SRzazewski <sz.rzazewski@gmail.com>
2024-05-24 22:51:18 +0200

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow{ { 800u, 600u }, "Arkanoid3000" };
+    window.setFramerateLimit(144);
+
+    sf::Text text;
+
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        window.close();
+    }
+
+    // select the font
+    text.setFont(font); // font is a sf::Font
+
+    // set the string to display
+    text.setString("Hello world in Arkanoid3000");
+
+    // set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+    // set the color
+    text.setFillColor(sf::Color::Red);
+
+    // set the text style
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        
+
+        window.clear();
+        // inside the main loop, between window.clear() and window.display()
+        window.draw(text);
+        window.display();
+    }
+}
