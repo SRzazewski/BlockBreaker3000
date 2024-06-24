a743c32936664e792c7b3a801b68a5f80494bf47
SRzazewski <sz.rzazewski@gmail.com>
2024-06-24 22:15:53 +0200

    physic_of_object was deleted

--- src/BlockBreaker3000.cpp
+++ src/BlockBreaker3000.cpp
@@ -1,7 +1,3 @@
-#include "paddle.hpp"
-#include "ball.hpp"
-#include "physic_of_object.hpp"
-#include "block.hpp"
 #include "game.hpp"
 #include <SFML/Graphics.hpp>
 #include <memory>
@@ -16,29 +12,16 @@ int main()
     auto window = sf::RenderWindow{ { static_cast<unsigned int>(block_breaker_area.x_stop), static_cast<unsigned int>(block_breaker_area.y_stop) }, "BlockBreaker3000" };
     window.setFramerateLimit(144);
 
-    game game_obj = game();
+    game game_obj = game(block_breaker_area);
 
-    // sf::Text text;
     sf::Font font;
     if (!font.loadFromFile("arial.ttf"))
     {
         window.close();
     }
-    // text.setFont(font);
-    // text.setString("BlockBreaker3000");
-    // text.setCharacterSize(24);
-    // text.setFillColor(sf::Color::Red);
-    // text.setStyle(sf::Text::Bold);
 
     sf::Clock clock_obj = sf::Clock();
     sf::Time time_obj = sf::Time();
-
-    sf::RectangleShape game_area_shape = sf::RectangleShape();
-    game_area_shape.setSize(sf::Vector2f(block_breaker_area.x_stop - block_breaker_area.x_start, block_breaker_area.y_stop - block_breaker_area.y_start));
-    game_area_shape.setFillColor(sf::Color::Black);
-    game_area_shape.setPosition(sf::Vector2f(block_breaker_area.x_start, block_breaker_area.y_start));
-
-    game_states game_state = game_states::during_game;
 
     game_obj.init(font);
 
@@ -50,7 +33,6 @@ int main()
         }
 
         window.clear(sf::Color(166u, 166u, 166u));
-        window.draw(game_area_shape);
         game_obj.update(clock_obj);
         game_obj.draw(window);
 
