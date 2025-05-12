b20ddc3cdaedffbe921aee05f05a17a76840bad8
SRzazewski <sz.rzazewski@gmail.com>
2025-05-12 21:15:43 +0200

    Fix run_built.py

--- run_built.py
+++ run_built.py
@@ -7,9 +7,13 @@ import os
 game_file_execution = "build/bin/BlockBreaker3000"
 game_file_execution_tests = "build/bin/BlockBreaker3000_tests"
 
+flags = [
+    "--clean", "-C" # clean built files
+    ]
+
 argument_list = sys.argv[1:]
 
-if argument_list:
+if argument_list and "test" == argument_list[0]:
     exe_file = game_file_execution_tests
 else:
     exe_file = game_file_execution
@@ -21,6 +25,6 @@ execution_command = "./" + exe_file
 if os.path.exists(exe_file):
     subprocess.run([execution_command])
 else:
-    subprocess.run(["cmake -S . -B build"])
-    subprocess.run(["make -C build"])
+    subprocess.run(["cmake -S . -B build"], shell = True, executable="/usr/bin/bash")
+    subprocess.run(["make -C build"], shell = True, executable="/usr/bin/bash")
     subprocess.run([execution_command])
